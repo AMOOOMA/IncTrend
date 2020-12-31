bd8fcbb5a224d5156812cd9ef363450b44878c3e
RuiLong J <jrl-china@hotmail.com>
2020-12-31 21:02:17 +0800

    Add api endpoint for Message model

--- IncTrend/nlp_api/views.py
+++ IncTrend/nlp_api/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework import viewsets
+
+from .serializers import MessageSerializer
+from .models import Message
+
+
+class MessageViewSet(viewsets.ModelViewSet):
+    queryset = Message.objects.all().order_by('type')
+    serializer_class = MessageSerializer
