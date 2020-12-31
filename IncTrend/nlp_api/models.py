4eabbaa2a8fd079e5d967c88814385a07dc56947
RuiLong J <jrl-china@hotmail.com>
2020-12-31 20:42:17 +0800

    Add Message model for teststing

--- IncTrend/nlp_api/models.py
+++ IncTrend/nlp_api/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+# Message model for testing purposes
+class Message(models.Model):
+    type = models.CharField(max_length=10)
+    body = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.type + ":" + self.body
